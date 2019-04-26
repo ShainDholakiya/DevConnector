4ca1cd3e7b5d2a74b0a63760f540d0b32a60699d
Shain Dholakiya <shainshain99@gmail.com>
2019-04-25 22:46:32 -0400

    login

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://shainshain99:123@devconnector-3mxtq.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://shainshain99:123@devconnector-3mxtq.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
